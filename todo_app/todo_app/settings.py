b89b9a6963835f49f73d3d6590d3790e4f4f2559
Munewxar <steven.altamirano.work@gmail.com>
2023-06-13 17:27:20 +0300

    bootstrap icons added

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "bootstrap4",
     "crispy_bootstrap4",
     "crispy_forms",
+    "django_bootstrap_icons",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
