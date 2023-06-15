7898efa10eaa1af068aedff9614aea1c72b696d3
Munewxar <steven.altamirano.work@gmail.com>
2023-06-15 20:16:13 +0300

    task related urls added

--- todo_app/todo/urls.py
+++ todo_app/todo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("tasks/", views.tasks, name="tasks"),
     path("new_task/", views.new_task, name="new_task"),
+    path("complete_task/<int:task_id>/", views.complete_task, name="complete_task"),
+    path("delete_task/<int:task_id>/", views.delete_task, name="delete_task"),
 ]
